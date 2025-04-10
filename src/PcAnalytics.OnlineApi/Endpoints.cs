50cd7f3690ee68f5611af9b34840c33ef1a9d8f4
Neville <neville@mediroutes.com>
2025-04-09 20:48:06 -0700

    setting up incoming

---
+++ src/PcAnalytics.OnlineApi/Endpoints.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PcAnalytics.Models;
+using PcAnalytics.ServerLogic;
+
+namespace PcAnalytics.OnlineApi
+{
+    public static class Endpoints
+    {
+
+
+        public static async Task AddIncomingAsync(HttpRequest request,
+                                                  IncomingSensorInput input,
+                                                  AppDbContext dbContext,
+                                                  CancellationToken cancellationToken = default)
+        {
+
+            if (request.Headers.TryGetValue("computerSerial", out var computerIds))
+            {
+                string serial = computerIds.First() ?? throw new Exception("No computer Id");
+
+                int computerId = await dbContext.Computers
+                                                .Where(c => c.Identifier == serial)
+                                                .Select(c => c.Id)
+                                                .SingleOrDefaultAsync(cancellationToken: cancellationToken);
+
+
+            }
+        }
+
+    }
+}
