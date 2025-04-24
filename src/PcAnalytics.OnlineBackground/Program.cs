075d15152f17c47e7a63064a6102589495f9a404
Neville Nazerane <live.neville@live.com>
2025-04-23 21:13:51 -0700

    setting up background

---
+++ src/PcAnalytics.OnlineBackground/Program.cs
@@ -0,0 +1,8 @@
+using PcAnalytics.OnlineBackground;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+
+await host.RunAsync();
