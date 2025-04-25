afb299b7896e73d299453c5c48f41a4f6d77fbe6
Neville Nazerane <live.neville@live.com>
2025-04-25 00:46:31 -0700

    updated starter

--- pipelines/local deploy.bat
+++ pipelines/local deploy.bat
@@ -1,2 +1,10 @@
-dotnet publish ..\src\PcAnalytics.ClientApi -o published\ClientApi
-dotnet publish ..\src\PcAnalytics.ClientBackground -o published\ClientBackground
+rmdir /s /q published
+mkdir published\ClientApi
+mkdir published\ClientBackground
+
+dotnet publish ..\src\PcAnalytics.ClientApi -c Release -o published\ClientApi
+dotnet publish ..\src\PcAnalytics.ClientBackground -c Release -o published\ClientBackground
+
+echo @echo off > published\run.bat
+echo start "" ClientApi\PcAnalytics.ClientApi.exe >> published\run.bat
+echo start "" ClientBackground\PcAnalytics.ClientBackground.exe >> published\run.bat
