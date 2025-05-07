06909ddc4db88f183a13b53bd7ef93dbc6731b81
Neville <neville@mediroutes.com>
2025-05-06 23:06:00 -0700

    publish folder changed

--- pipelines/local deploy.bat
+++ pipelines/local deploy.bat
@@ -1,16 +1,16 @@
-rmdir /s /q published
-mkdir published\ClientApi
-mkdir published\ClientBackground
+rmdir /s /q analytics
+mkdir analytics\ClientApi
+mkdir analytics\ClientBackground
 
-dotnet publish ..\src\PcAnalytics.ClientApi -c Release -o published\ClientApi
-dotnet publish ..\src\PcAnalytics.ClientBackground -c Release -o published\ClientBackground
+dotnet publish ..\src\PcAnalytics.ClientApi -c Release -o analytics\ClientApi
+dotnet publish ..\src\PcAnalytics.ClientBackground -c Release -o analytics\ClientBackground
 
 echo @echo off > published\run.bat
-echo start "" /min ClientApi\PcAnalytics.ClientApi.exe >> published\run.bat
-echo start "" /min ClientBackground\PcAnalytics.ClientBackground.exe >> published\run.bat
+echo start "" /min ClientApi\PcAnalytics.ClientApi.exe >> analytics\run.bat
+echo start "" /min ClientBackground\PcAnalytics.ClientBackground.exe >> analytics\run.bat
 
 if exist configs.json (
-    copy /Y configs.json published\configs.json
+    copy /Y configs.json analytics\configs.json
 ) else (
     > published\configs.json (
         echo {
